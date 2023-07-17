1808212b88acd4640fb5d8778ae5b3d138de6a3f
Muktar16 <bsse1116@iit.du.ac.bd>
2023-07-17 13:43:20 +0600

    hi

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function moreDesignation() { 
-    document.getElementById("hideDesignation").innerHTML = "Expert coder in Codeforces<br>5 star coder in Codechef<br>Knight coder in Leetcode";
+    document.getElementById("hideDesignation").innerHTML = "React Devoloper<br>Node Js Developer<br>";
 }
 
 function moreDescribe() { 
-    document.getElementById("hideDescribe").innerHTML = " I am interested in competitive programming.";
+    document.getElementById("hideDescribe").innerHTML = "I am interested in machine learning.";
 }
 
 function downloadPDF() {
     const url = '';
     const link = document.createElement('a');
     link.href = url;
-    link.download = 'my-pdf-file.pdf';
+    link.download = 'MUKTAR_CV.pdf';
     link.click();
 }
